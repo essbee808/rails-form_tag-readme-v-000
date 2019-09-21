b239c5af59558c502d94d2bdfcce863bad9879cb
Sheena Bonilla <sheena.myb@gmail.com>
2019-09-21 06:55:46 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -15,5 +15,7 @@ describe 'new post' do
     visit new_post_path
     fill_in 'post_title', with: 'My post title'
     fill_in 'post_description', with: 'My post description'
+
+    click_on 'Submit Post'
   end
 end
